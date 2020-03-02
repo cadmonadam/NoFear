d2442c2421a960ac58462aa5ff157cfb812156af
cadmonadam <cadmonadam@gmail.com>
2020-03-02 22:20:44 +0100

    Setting up onClickListener interfaces instead of onClick methods.

--- app/src/main/java/com/example/android/nofear/MainActivity.java
+++ app/src/main/java/com/example/android/nofear/MainActivity.java
@@ -5,18 +5,57 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton imageButton1;
+    private ImageButton imageButton2;
+    private ImageButton imageButton3;
+    private TextView address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton1 = findViewById(R.id.imageButton1);
+        imageButton2 = findViewById(R.id.imageButton2);
+        imageButton3 = findViewById(R.id.imageButton3);
+        address = findViewById(R.id.android_address_texview);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber();
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openG();
+            }
+        });
+
+        address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMap();
+            }
+        });
     }
 
-    public void OpenG(View view) {
+    public void openG() {
         Uri webpage = Uri.parse("https://plus.google.com/u/0/+Gerg%C5%91Siska");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -25,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void SendEmail(View view) {
+    public void sendEmail() {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto: vreal@gmail.com")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_SUBJECT, "Inquiry");
@@ -35,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void dialPhoneNumber(View view) {
+    public void dialPhoneNumber() {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:123456789"));
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -43,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void showMap(View view) {
+    public void showMap() {
         Uri gmmIntentUri = Uri.parse("geo:48.05624,22.22206");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
